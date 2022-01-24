d3c8d92375686e961ec523cd3682ae62d4fff172
umit <umit@appyventures.com>
2022-01-24 22:32:28 +0300

    updated text font family

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'colors.dart';
 import 'font_family.dart';
 
 final ThemeData themeData = ThemeData(
-  fontFamily: FontFamily.rubik,
+  fontFamily: GoogleFonts.inter().fontFamily,
   brightness: Brightness.light,
   primarySwatch: MaterialColor(AppColors.red[900]!.value, AppColors.red),
   primaryColor: AppColors.red[900],
